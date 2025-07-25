6c375a2a2775edb57005c0fe94c25fdf22050bbb
Keetapat33333 <>
2025-07-25 11:47:06 +0700

    update Variable

---
+++ VariableExample.java
@@ -0,0 +1,28 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        // เก็บค่าตัวเลขจำนวนเต็ม
+        int websiteVisitors = 0;
+
+        // เก็บค่าตัวเลขทศนิยม ลงท้ายด้วยfด้วยนะจ๊ะ
+        float annualExapenses = 100.222f;
+
+        // เก็บค่าตัวเลขเป็นทศนิยม
+        double studentGPA = 4.0;
+
+        // เก็บค่าตัวเลขเป็นทศนิยม แต่เปลี่ยนค่าไม่ได้
+        final double PI = 3.14;
+
+        // เก็บค่าเป็นข้อความ1ตัว
+        char text = 't';
+
+        // เก็บค่าจริงเท็จ
+        boolean test = true;
+
+        System.out.println(websiteVisitors);
+        System.out.println(annualExapenses);
+        System.out.println(studentGPA);
+        System.out.println(PI);
+        System.out.println(text);
+        System.out.println(test);
+    }
+}
