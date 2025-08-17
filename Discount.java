6922d4f00bf2d154912378b4f2e67739215a5881
Keetapat333 <keetapat369@gmail.com>
2025-08-17 19:03:45 +0700

    update

--- Discount.java
+++ Discount.java
@@ -1,15 +1,26 @@
+
 import java.util.Scanner;
 
-public class BodySurfaceArea {
+public class Discount {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter Price :");
-        int Price = scanner.nextInt();
-        System.out.print("Enter number to buy");
-        int buy = scanner.nextInt();
-        System.out.print("Enter discount:");
-        int discount = scanner.nextInt();
- 
+        System.out.print("Enter Price: ");
+        int xPrice = scanner.nextInt();
+        System.out.print("Enter number to buy: ");
+        int xBuy = scanner.nextInt();
+        System.out.print("Enter discount: ");
+        double xDiscount = scanner.nextDouble();
+        double sumDiscount = xDiscount / 100.0;
+
+        int total = xPrice * xBuy;
+        System.out.println("Total cost = " + total + " Baht");
+
+        double discountAmount = total * sumDiscount;
+        System.out.println("discountAmount = " + discountAmount + " Baht");
+
+        double netPay = total - discountAmount;
+        System.out.println("Net Pay = " + netPay + " Baht");
     }
 }
